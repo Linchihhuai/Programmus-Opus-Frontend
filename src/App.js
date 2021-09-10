f77256b7cadfc9a19fe9d2652e22fe68388027a3
Chih Huai Lin <chihhuai.lin@student.vinci.be>
2021-09-10 17:46:42 +0200

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import backgroundImg from './img/login.jpg';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="loginPage">
+      <head>
+        <title>Programmus Opus</title>
+      </head>
+      <body>
+        <img src={backgroundImg} alt="backgroundImg" />
+        
+        <div class = "quote">
+          <p>"Not all those who wander are lost."</p>
+          <p>- J.R.R. Tolkien, The Fellowship of the Ring</p>
+        </div>
+
+        <div class = "loginForm">
+          <input type ="text" id="magicWord" size="50"  placeholder="Enter the magic words to access the Programmus Opus!"></input><br></br>
+          <button type="submit" id="loginSubmit">Submit</button>
+        </div>
+      </body>
     </div>
   );
 }
